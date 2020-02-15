28b9259428b52efa2c1a551e3f07389340076a1b
Neylson Crepalde <neylsoncrepalde@gmail.com>
2020-02-15 13:21:46 -0200

    comecei script para limpar dados

---
+++ src/limpa_dados.py
@@ -0,0 +1,40 @@
+import sys
+
+PERGUNTA = "Você gostaria de deletar os dados não processados da RAIS do seu computador?"
+
+def query_yes_no(question, default="no"):
+    """Ask a yes/no question via input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+if __name__ == "__main__":
+    print(query_yes_no(PERGUNTA))
+    
